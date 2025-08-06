325e344e945be7f0091e1cc6a6ef3d8c67b35aba
sf <srfenton@protonmail.com>
2025-08-06 07:40:18 -0700

    fixed some typos in the vocab and updated the app to take .erb instead of directly injecting content.

--- app.rb
+++ app.rb
@@ -1,27 +1,12 @@
-file_path = File.join(__dir__,'lezioni.tsv')
-parole = []
-
-File.open(file_path, 'r') do |file|
-  # Skip the header row
-  header = file.gets
-  file.each_line do |line|
-   # Split the line by tab character
-    parts = line.split("\t") 
-
-    # Extract verb translation and verb forms from .txt
-    parole.push("#{parts[0].strip} : #{parts[1].strip}")
-     
-
-
-
-     end
-    # store conjugation data w/ the translation as the key 
-end
-
-
 require 'sinatra'
 
+parole = File.readlines("lezioni.tsv")[1..].map do |line|
+  parts = line.split("\t")
+  "#{parts[0].strip} : #{parts[1].strip}" if parts[1]
+end.compact
+
 get '/' do
-  parole.sample(10).join("<br>")
+  @words = parole.sample(10)
+  erb :index
 end
 
